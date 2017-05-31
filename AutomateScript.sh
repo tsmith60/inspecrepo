738408342b4c323b6f6a0c97db5bd795c006e247
Ted Smith III <tsmith60@students.towson.edu>
2017-05-31 03:53:54 -0400

    Shutdown 3
    I need the failure report to get sent successfully before shutting down

--- AutomateScript.sh
+++ AutomateScript.sh
@@ -28,7 +28,7 @@ if [ -n "$report" ]; then
                     #Basically means if the fail report was created, execute below
                      echo "Report found"
                    logger Report inspec failures on system $( hostname)
-                   sudo shutdown -h now
+                   sudo shutdown -h
                         else
                 #Basically means if there is a success, execute below
                     echo "All Successful"
